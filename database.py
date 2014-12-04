c872013c178fabf4149d5da78594a9761debfcea
Aaron Joyce <joyceaa@tcd.ie>
2014-12-04 18:28:04 +0000

    Create database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+# Just to assist prototyping; not the actual database.
+# Ciaran: Please delete as soon as add a version of the 
+# actual Database ADT.  
+
+class Database:
+	def __init__( self ):
+		pass
+
+	def get_state( self, block_id ):
+		pass
+
+	def update_state( self, block_id, pos ):
+		pass
+
+	def read_into_db( self, filename ):
+		pass
+
+	def get_data( self, block_id ):
+		pass
+
+	def data_available( self ):
+		pass
