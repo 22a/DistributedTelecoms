daf47cc0e44357df68960db21bea508ad31318bd
Aaron Joyce <joyceaa@tcd.ie>
2014-12-05 11:06:09 +0000

    Create worker_pool_tests.py

---
+++ worker_pool_tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+class TestWorkerPoolBehaviour(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_initialiser(self):
+		pass
+
+	def test_empty(self):
+		pass
+
+	def test_size(self):
+		pass
+
+	def test_reorder(self):
+		pass
+
+	def test_resize(self):
+		pass
+
+	def test_insert(self):
+		pass
+
+	def test_delete_min(self):
+		pass
+
+	def test_dequeue(self):
+		pass
+
+	def test_greater(self):
+		pass
+
+	def test_is_minheap(self):
+		pass
+
+	def test_get_capacity(self):
+		pass
+
+	def test_key_generator(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
