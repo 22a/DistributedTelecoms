341ae86597bad9fd7d5a9e1236f0f157b67d6893
Aaron Joyce <joyceaa@tcd.ie>
2014-12-05 10:53:44 +0000

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,3 +1,7 @@
 class EmptyQueueException( Exception ):
 	def __init__( self, message ):
 		super( EmptyQueueException, self ).__init__( message )
+
+class WorkerUnavailableException( Exception ):
+	def __init__( self, message ):
+		super( WorkerUnavailableException, self ).__init__( message )
