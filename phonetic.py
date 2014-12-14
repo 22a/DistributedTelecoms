de60fd6f8e6596740612da018adae12d7ef644b2
Peter Meehan <meehanp2@tcd.ie>
2014-12-14 19:14:21 +0000

    fixed bugs in phonetic comparison

--- phonetic.py
+++ phonetic.py
@@ -1,4 +1,6 @@
 #super quick stab at the phoenetic string representation builder
+#only works on upercase for the minute, can be easily extended to lowercase
+#perhaps the query only be run on either first or last name, I have no incoding for space
 
 def phonetify(name = "TWEEZER"):
   out = ''
@@ -11,43 +13,39 @@ def phonetify(name = "TWEEZER"):
   # 6 = 'R'
 
   #replace all chars with grouping num
-  for c in range(0, len(name)):
-    if name[c] in t0:
+  for c in name:
+    if c in t0:
       out+='0'
-    elif name[c] in t1:
+    elif c in t1:
       out+='1'
-    elif name[c] in t2:
+    elif c in t2:
       out+='2'
-    elif name[c] in t3:
+    elif c in t3:
       out+='3'
-    elif name[c] == 'L':
+    elif c == 'L':
       out+='4'
-    elif name[c] in t5:
+    elif c in t5:
       out+='5'
-    elif name[c] == 'R':
+    elif c == 'R':
       out+='6'
     else:
       out+='?'
 
-
-  print 'out =',out
-
   #remove all adjacent duplicates
-  for i in range(len(out)):         #this will probably go out of bounds
+  for i in range(len(out)):
     if i is not len(out)-1 and out[i+1] == out[i]:
       print(i)    
       out = out.replace(out[i], '', 1)
 
 
   for i in range(len(out)):
-    if out[i] is not '0':
-      for c in range(i, len(out)):   
-        out = out[:i-1] + out[i:].replace('0', '')    #replace all non leading zeros
-        break
+    if out[i] is not '0':  
+      out = out[:i] + out[i:].replace('0', '')    #replace all non leading zeros
+      break
 
   out = out[:4]     #reduce to 4 chars
 
-  out[0] = name[0]    #replace leading digit with first letter of name
+  out = name[0] + out[1:]  #replace leading digit with first letter of name
 
   return out
 
