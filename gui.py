d8ddf8666291eb9f5b5aaac2a8abc14499b4ba50
Peter Meehan <meehanp2@tcd.ie>
2014-12-19 02:04:41 +0000

    updated to web UI

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 #These function can be merged into server.py, that way we can jsonify the simple returns 
 #from the db and various pools
 
+import webbrowser
 from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
@@ -10,12 +11,21 @@ def return_bPool():
 
 @app.route('/_return_bPro')
 def return_bPro():
-
   return jsonify(result=15200)
 
 @app.route('/_return_wPool')
 def return_wPool():
   return jsonify(result=40)
+
+@app.route('/_return_fProg')
+def return_fProg():
+  return jsonify(result=90)
+
+@app.route('/_return_fCread')
+def return_fCread():
+  ## we should have a var here that is 1 if we're reading in a file, 0 otherwise
+  return jsonify(result=1)
+
     
 @app.route("/")
 def hello():
@@ -23,4 +33,6 @@ def hello():
 
 if __name__ == "__main__":
   app.debug = True
+  #b = webbrowser.get('firefox')
+  #b.open('127.0.0.1:5000')       #using webbrowser to automatically open the ui
   app.run()
