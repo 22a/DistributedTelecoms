86c7d432dc63c53e3c9f80bc219e35d2d709dccb
Aaron Joyce <joyceaa@tcd.ie>
2014-12-19 19:11:25 +0000

    Update packets.py

--- packets.py
+++ packets.py
@@ -12,26 +12,31 @@ def processed_section(block_id, position):
 	packet_contents = { 'type'		: '1000_PROCESSED', 
 						'block_id'	: block_id,
 						'position'	: position }
-	return json.dumps(packet_contents)
+	return json.dumps([packet_contents], separators=(',', ': '))
 
-def completed(block_id):
-	packet_contents = { 'type'		: 'COMPLETED', 
+def complete(block_id):
+	packet_contents = { 'type'		: 'COMPLETE', 
 						'block_id'	: block_id }
-	return json.dumps(packet_contents)
+	return json.dumps([packet_contents])
 
 # Currently busy processing block block_id
 def busy(block_id):
-	packet_contents = { 'type'		: 'BUSY', 
-						'block_id'	: block_id }
-	return json.dumps(packet_contents)
+	packet_contents = []
+	packet_contents.append( { 'type' : 'BUSY' } )
+	packet_contents.append( { 'block_id' : block_id } ) 
+	return json.dumps(packet_contents, sort_keys = True)
 
-def dataset(block_id, start_index, target, payload):
-	packet_contents = { 'type'			: 'DATA', 
-						'block_id'		: block_id,
-						'start_index'	: start_index, 
-						'target'		: target,
-						'payload'		: payload }
-	return json.dumps(packet_contents)
+
+def dataset(block_id, start_index, target, payload, more):
+	packet_contents = []
+	packet_contents.append( { 'type' : 'DATA' } )
+	packet_contents.append( { 'block_id' : block_id } )
+	packet_contents.append( { 'start_index' : start_index } )
+	packet_contents.append( { 'target' : target } )
+	packet_contents.append( { 'payload' : payload } )
+	packet_contents.append( { 'more' : more } )
+	return json.dumps(packet_contents, sort_keys = True, ensure_ascii = True)
+
 
 # Sent from a Client (@ id) to the Server/Coordinator, 
 # informing it (the Server) whether it is available to process data
