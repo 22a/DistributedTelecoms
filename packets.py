d6a694f2c8b2bb717dec63458c3b6863e0e49955
Aaron Joyce <joyceaa@tcd.ie>
2015-01-12 17:28:38 +0000

    Update packets.py

--- packets.py
+++ packets.py
@@ -12,7 +12,7 @@ def processed_section(block_id, position):
 	packet_contents = { 'type'		: '1000_PROCESSED', 
 						'block_id'	: block_id,
 						'position'	: position }
-	return json.dumps([packet_contents], separators=(',', ': '))
+	return json.dumps(packet_contents, separators=(',', ': '))
 
 def complete(block_id):
 	packet_contents = { 'type'		: 'COMPLETE', 
@@ -21,25 +21,28 @@ def complete(block_id):
 
 # Currently busy processing block block_id
 def busy(block_id):
-	packet_contents = []
-	packet_contents.append( { 'type' : 'BUSY' } )
-	packet_contents.append( { 'block_id' : block_id } ) 
+	packet_contents = { 'type' : 'BUSY', 
+						'block_id' : block_id } 
 	return json.dumps(packet_contents, sort_keys = True)
 
 
 def dataset(block_id, start_index, target, payload, more):
-	packet_contents = []
-	packet_contents.append( { 'type' : 'DATA' } )
-	packet_contents.append( { 'block_id' : block_id } )
-	packet_contents.append( { 'start_index' : start_index } )
-	packet_contents.append( { 'target' : target } )
-	packet_contents.append( { 'payload' : payload } )
-	packet_contents.append( { 'more' : more } )
+	packet_contents = {	'type' : 'DATA', 
+						'block_id' : block_id, 
+						'start_index' : start_index, 
+						'target' : target, 
+						'payload' : payload, 
+						'more' : more }
 	return json.dumps(packet_contents, sort_keys = True, ensure_ascii = True)
 
 
 # Sent from a Client (@ id) to the Server/Coordinator, 
 # informing it (the Server) whether it is available to process data
+
+def request_accepted():
+	packet_contents = { 'type'		: "REQUEST_ACCEPTED" }
+	return json.dumps(packet_contents)
+
 def available(id, availability):
 	packet_contents = { 'type'		: 'STATUS', 
 						'id'		: id, 
@@ -48,11 +51,38 @@ def available(id, availability):
 
 
 # Sent from the Server/Coordinator to an arbitrary client
-def available():
-	packet_contents = { 'available'		: 'AVAILABILITY' }
+def available(packet_count):
+	packet_contents = { 'type'		: 'AVAILABLE', 
+						'packet_count'	: packet_count }
+	return json.dumps(packet_contents)
+
+def connect(id):
+	packet_contents = { 'type'		: 'INCOMING_CONNECTION', 
+						'id'		: id }
 	return json.dumps(packet_contents)
 
 
+def positive_data_acknowledgement(block_id, start_index):
+	packet_contents = { 'type'			: 'POS_DATA_ACK', 
+						'block_id'		: block_id, 
+						'start_index'	: start_index }
+	return json.dumps(packet_contents)
 
+def ping():
+	packet_contents = { 'type'			: 'PING' }
+	return json.dumps(packet_contents)
+
+def alive():
+	packet_contents = { 'type'			: 'ALIVE' }
+	return json.dumps(packet_contents)
+
+def match_request():
+	packet_contents = { 'type'			: 'MATCH_REQUEST'}
+	return json.dumps(packet_contents)
+
+def match_found(matches):
+	packet_contents = { 'type'			: 'MATCH_FOUND', 
+						'matches'		: matches }
+	return json.dumps(packet_contents)
 
 
